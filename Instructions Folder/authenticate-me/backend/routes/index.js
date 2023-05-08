5986aac9fa0070e1302fe22b0d775a69d3e5c68b
Chris <andre89gomez@gmail.com>
2023-05-08 16:50:40 -0700

    Update

--- Instructions Folder/authenticate-me/backend/routes/index.js
+++ Instructions Folder/authenticate-me/backend/routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const apiRouter = require('./api');
 
 
+router.use('/api', apiRouter);
+
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
@@ -12,6 +14,5 @@ router.get("/api/csrf/restore", (req, res) => {
   });
 });
 
-router.use('/api', apiRouter);
 
 module.exports = router;
