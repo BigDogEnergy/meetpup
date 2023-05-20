a1cf0b99570ce9b98935b261f224ec2efa8a57fa
Chris <andre89gomez@gmail.com>
2023-05-20 00:25:16 -0700

    manual date setting

--- backend/db/seeders/20230520063017-venues.js
+++ backend/db/seeders/20230520063017-venues.js
@@ -19,6 +19,8 @@ module.exports = {
         state: "test",
         lat: 33.3333333,
         lng: 22.2222222,
+        createdAt: currentDate,
+        updatedAt: currentDate,
       },
     ], options);
 
