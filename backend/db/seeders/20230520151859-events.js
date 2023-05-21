a7b3dc8daf0155854fc1974bff9e7d3a8710ca82
Chris <andre89gomez@gmail.com>
2023-05-20 17:09:21 -0700

    added event seeder data

--- backend/db/seeders/20230520151859-events.js
+++ backend/db/seeders/20230520151859-events.js
@@ -20,6 +20,38 @@ module.exports = {
         startDate: "2023-06-01 07:00:00",
         endDate: "2023-07-01 07:00:00",
       },
+      {
+        groupId: 2,
+        venueId: 2,
+        name: "Group 2 / Venue 2 name",
+        type: "Online",
+        startDate: "2023-08-01 07:00:00",
+        endDate: "2023-09-01 07:00:00",
+      },
+      {
+        groupId: 3,
+        venueId: 3,
+        name: "Group 3 / Venue 3 name",
+        type: "Online",
+        startDate: "2023-09-01 07:00:00",
+        endDate: "2023-10-01 07:00:00",
+      },
+      {
+        groupId: 4,
+        venueId: 4,
+        name: "Group 4 / Venue 4 name",
+        type: "Online",
+        startDate: "2023-10-01 07:00:00",
+        endDate: "2023-11-01 07:00:00",
+      },
+      {
+        groupId: 5,
+        venueId: 5,
+        name: "Group 5 / Venue 5 name",
+        type: "Online",
+        startDate: "2023-11-01 07:00:00",
+        endDate: "2023-12-01 07:00:00",
+      },
     ], options);
 
   },
