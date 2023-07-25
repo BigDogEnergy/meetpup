9c6cbba693028f9bd3069c9b88b8dde9a38cd539
Chris <andre89gomez@gmail.com>
2023-07-24 17:40:21 -0700

    signup feature started

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -19,7 +19,6 @@ const removeUser = () => {
 // THUNKS
 
 // Login thunk action
-
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch("/api/session", {
@@ -34,12 +33,37 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+// Restore User thunk action
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+// Signup User thunk action
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+// INITIAL STATE
 const initialState = { user: null };
 
 
-
 //REDUCER
-
 const sessionReducer = (state = initialState, action) => {
   let newState;
   switch (action.type) {
