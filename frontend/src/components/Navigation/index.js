5b194509447a97a4b6070b6d91cc4e759b0f7b98
Chris <andre89gomez@gmail.com>
2023-07-27 16:54:38 -0700

    landing page and login

---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import './Navigation.css';
+
+function Navigation({ isLoaded }){
+  const sessionUser = useSelector(state => state.session.user);
+
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
+      <li>
+        <ProfileButton user={sessionUser} />
+      </li>
+    );
+  } else {
+    sessionLinks = (
+      <li>
+        <div className="nav-links-container">
+            <div className="login">
+                <NavLink to="/login">Log In</NavLink>
+            </div>
+            <div className="signup">
+                <NavLink to="/signup">Sign Up</NavLink>
+            </div>
+        </div>
+      </li>
+    );
+  }
+
+  return (
+    <ul>
+      <li>
+        <NavLink exact to="/">Home</NavLink>
+      </li>
+      {isLoaded && sessionLinks}
+    </ul>
+  );
+}
+
+export default Navigation;
