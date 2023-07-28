8523d318f60588f9e4b852464d5ab091679a3a07
Chris <andre89gomez@gmail.com>
2023-07-27 17:01:31 -0700

    trying to fix render bugs again 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,6 +24,9 @@ function App() {
           <Route path="/signup">
             <SignupFormPage />
           </Route>
+          <Route path="/">
+            <h1>Root</h1>
+          </Route>
         </Switch>
       )}
     </>
